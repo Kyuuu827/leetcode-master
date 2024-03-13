7853aa1f83f7c03689dead0b00f126c365c1e24e
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-03-13 15:07:02 +0900

    Time: 3 ms (7.45%), Space: 40.9 MB (46.91%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,26 +1,25 @@
 class Solution {
     public int reverse(int x) {
-        String str = String.valueOf(x);
-        String resultStr ="";
-        
-        String answer ="";
-        
-        boolean minusflag = false;
-        
-        if(str.charAt(0) == '-') {
-            minusflag = true;
-            str = str.substring(1); // -를 제외
-        }
-        
-        for(int i=str.length()-1; i >=0; i--){
-            resultStr += String.valueOf(str.charAt(i));
-        }
-        
-        Long resultLong = Long.parseLong(resultStr);
-        if(minusflag) resultLong *= -1;
-        
-        if(resultLong > Integer.MAX_VALUE || resultLong < Integer.MIN_VALUE) { return 0; } // max값 범위 체크
+        try {
+            if (Math.pow(2, 31) -1 < x || x < Math.pow(-2, 31)) return 0;
 
-        return resultLong.intValue();
+            StringBuilder sb = new StringBuilder();
+
+            if (x > 0) {
+                String value = String.valueOf(x);
+                return Integer.parseInt(sb.append(value).reverse().toString());
+            }
+
+            if (x < 0) {
+                String value = String.valueOf(x);
+                return Integer.parseInt("-" + sb.append(value.substring(1)).reverse());
+
+            }
+
+            return 0;
+        }
+        catch (NumberFormatException e) {
+            return 0;
+        }
     }
 }
