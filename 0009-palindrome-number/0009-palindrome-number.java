48f5421eed239631d39a027b042b183049cd28cd
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-03-26 22:24:26 +0900

    Time: 7 ms (18.89%), Space: 43.8 MB (75.56%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0 || (x != 0 && x % 10 == 0)) { return false; }
+        
         String xStr = String.valueOf(x);
         StringBuilder sb = new StringBuilder();
         sb.append(xStr);
+        
         return sb.reverse().toString().equals(xStr);
 
     }
