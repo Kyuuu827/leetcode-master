8a30de36c430aaa0101e5934f07ea770aca93308
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-07-14 00:48:58 +0900

    Time: 1683 ms (5.03%), Space: 0B (100.00%) - LeetHub

---
+++ 1965-employees-with-missing-information/1965-employees-with-missing-information.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+SELECT e.employee_id 
+FROM Employees e
+LEFT JOIN Salaries as s USING(employee_id)
+WHERE salary IS NULL
+UNION
+SELECT s.employee_id 
+FROM Employees e
+RIGHT JOIN Salaries as s USING(employee_id)
+WHERE name IS NULL
+ORDER BY employee_id
