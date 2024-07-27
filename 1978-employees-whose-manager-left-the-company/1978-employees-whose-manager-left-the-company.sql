cc7e57ea05824dc53968c835bd5b3de663099e91
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-07-28 00:41:04 +0900

    Time: 296 ms (84.11%), Space: 0B (100.00%) - LeetHub

---
+++ 1978-employees-whose-manager-left-the-company/1978-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT e.employee_id
+FROM Employees e
+LEFT JOIN Employees e2 ON e2.employee_id = e.manager_id
+WHERE e.salary < 30000 AND e.manager_id IS NOT NULL AND e2.employee_id IS NULL
+ORDER BY e.employee_id;
+    
