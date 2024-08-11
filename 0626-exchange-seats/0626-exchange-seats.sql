f4a836c054cbf475115174cf6f17c09b4db1a3f2
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-08-11 23:07:07 +0900

    Time: 329 ms (53.78%), Space: 0B (100.00%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,18 +1,3 @@
-# Write your MySQL query statement below
-# WITH cte AS (
-#     SELECT *, COUNT(*) OVER () AS total
-#     FROM Seat
-# )
-# SELECT CASE
-#             WHEN MOD(id, 2) = 1 AND id != total THEN id + 1
-#             WHEN MOD(id, 2) = 1 AND id = total THEN id
-#             ELSE id - 1
-#         END as id
-#         , student
-# FROM cte
-# ORDER BY id;
-
-# 서브쿼리만 사용
 SELECT CASE
             WHEN MOD(id, 2) = 1 AND id != total THEN id + 1
             WHEN MOD(id, 2) = 1 AND id = total THEN id
