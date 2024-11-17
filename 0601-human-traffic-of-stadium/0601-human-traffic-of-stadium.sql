e6b017b2cc8231b44a220aaac29347151d22f2e0
Lee, Chankyu <80111297+Kyuuu827@users.noreply.github.com>
2024-11-18 00:16:37 +0900

    Time: 401 ms (63.89%), Space: 0B (100.00%) - LeetHub

---
+++ 0601-human-traffic-of-stadium/0601-human-traffic-of-stadium.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+WITH base AS(
+    SELECT *
+         , LAG(id, 2) OVER (ORDER BY id) AS pre_pre_id
+         , LAG(id, 1) OVER (ORDER BY id) AS pre_id
+         , LEAD(id, 1) OVER (ORDER BY id) AS next_id
+         , LEAD(id, 2) OVER (ORDER BY id) AS next_next_id
+    FROM stadium
+    WHERE people >= 100
+)
+
+SELECT id
+     , visit_date
+     , people
+FROM base
+WHERE (pre_pre_id + 1 = pre_id AND pre_id + 1 = id)
+   OR (pre_id + 1 = id AND id + 1 = next_id)
+   OR (id + 1 = next_id AND next_id + 1 = next_next_id)
+ORDER BY visit_date;
